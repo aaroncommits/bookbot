272bd02afef63c1dfefbee0575d9da741e1a8f02
aaroncommits <aaronaccounts@pm.me>
2024-11-12 14:21:59 -0800

    filtered out symbols, formatted count into lines, and formatted full report

--- main.py
+++ main.py
@@ -10,4 +10,31 @@ if __name__ == "__main__":
 
 words = file_contents.split()
 
-print(len(words))
+char_count = {}
+lowercase_contents = file_contents.lower()
+
+for character in lowercase_contents:
+    if character in char_count:
+        char_count[character] += 1
+    else: 
+        char_count[character] = 1
+
+char_list = []
+for char, count in char_count.items():
+    if char.isalpha():
+        char_list.append({"char": char, "count": count})
+
+def sort_on(dict):
+    return dict["count"]
+
+char_list.sort(reverse=True, key=sort_on)
+
+
+print("--- Begin report of books/frankenstein.txt ---")
+print(f"{len(words)} words found in the document")
+print()
+
+for letter in char_list:
+    print(f"The '{letter['char']}' character was found {letter['count']} times")
+
+print("--- End report ---")
